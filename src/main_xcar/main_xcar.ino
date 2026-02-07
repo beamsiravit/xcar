aabb02d74e735c43be236ec5c697330b397e6a45
beamsiravit <beam.siravit.meeprasert@gmail.com>
2026-02-07 10:33:22 +0700

    Initial code

---
+++ src/main_xcar/main_xcar.ino
@@ -0,0 +1,18 @@
+void setup() {
+  pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(6,INPUT);
+  pinMode(7,INPUT);
+  pinMode(8,INPUT);
+
+}
+
+void loop() {
+
+digitalWrite(3,HIGH);
+digitalWrite(4,HIGH);
+  
+
+}
