607eb6ff11cddf0257247b4cd286bed43f698f8a
beamsiravit <beam.siravit.meeprasert@gmail.com>
2026-02-07 23:06:28 +0700

    Add IR remote

--- src/main_xcar/main_xcar.ino
+++ src/main_xcar/main_xcar.ino
@@ -1,18 +1,41 @@
-void setup() {
-  pinMode(2,OUTPUT);
-  pinMode(3,OUTPUT);
-  pinMode(4,OUTPUT);
-  pinMode(5,OUTPUT);
-  pinMode(6,INPUT);
-  pinMode(7,INPUT);
-  pinMode(8,INPUT);
+#include <Arduino.h>
+#include "PinDefinitionsAndMore.h" // Part of the IRremote library
+#include <IRremote.hpp>
 
+
+
+void setup() {
+
+    Serial.begin(115200);
+    // Start the receiver
+    IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);
+    pinMode(3,OUTPUT);
+    pinMode(4,OUTPUT);
+    pinMode(5,OUTPUT);
+    pinMode(6,OUTPUT);
 }
 
 void loop() {
+    if (IrReceiver.decode()) {
+        
+        uint16_t command = IrReceiver.decodedIRData.command;
 
-digitalWrite(3,HIGH);
-digitalWrite(4,HIGH);
-  
+        switch (command)
+        {
+          case 0x18:
+            Serial.print("UP");
+            break;
+          case 0x52:
+            Serial.print("DOWN");
+            break;
+          case 
+        }
 
+        
+
+        // Print the full raw data
+        //IrReceiver.printIRResultShort(&Serial); 
+        
+        IrReceiver.resume(); // Enable receiving of the next value
+    }
 }
