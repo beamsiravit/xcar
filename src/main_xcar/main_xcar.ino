bb8095eb24b2ac87119ad2f83244007398e9ac3b
beamsiravit <beam.siravit.meeprasert@gmail.com>
2026-02-08 00:05:54 +0700

    main progaramn

--- src/main_xcar/main_xcar.ino
+++ src/main_xcar/main_xcar.ino
@@ -9,10 +9,67 @@ void setup() {
     Serial.begin(115200);
     // Start the receiver
     IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);
-    pinMode(3,OUTPUT);
-    pinMode(4,OUTPUT);
-    pinMode(5,OUTPUT);
-    pinMode(6,OUTPUT);
+    pinMode(PIN_D3, OUTPUT);
+    pinMode(PIN_D4, OUTPUT);
+    pinMode(PIN_D5, OUTPUT);
+    pinMode(PIN_D6, OUTPUT);
+
+    digitalWrite(PIN_D3, LOW);
+    digitalWrite(PIN_D4, LOW);
+    digitalWrite(PIN_D5, LOW);
+    digitalWrite(PIN_D6, LOW);
+}
+
+void stop(){
+  digitalWrite(PIN_D3, LOW);
+  digitalWrite(PIN_D4, LOW);
+  digitalWrite(PIN_D5, LOW);
+  digitalWrite(PIN_D6, LOW);
+
+}
+  
+ 
+
+
+void driveMotor(int order)
+{
+  /*
+    1 = Forward
+    2 = Right
+    3 = Backward
+    4 = Left
+  */
+
+  digitalWrite(PIN_D3, LOW);
+  digitalWrite(PIN_D4, LOW);
+  digitalWrite(PIN_D5, LOW);
+  digitalWrite(PIN_D6, LOW);
+
+  switch (order)
+  {
+    case 1:
+      digitalWrite(PIN_D3, HIGH);
+      digitalWrite(PIN_D6, HIGH);
+      delay(1000);
+      stop();
+      break;
+    case 2:
+      digitalWrite(PIN_D4, HIGH);
+      digitalWrite(PIN_D5, HIGH);
+      delay(1000);
+      stop();
+      break;
+    case 3:
+      digitalWrite(PIN_D3, HIGH);
+      digitalWrite(PIN_D5, HIGH);
+      break;
+    case 4:
+      digitalWrite(PIN_D4, HIGH);
+      digitalWrite(PIN_D6, HIGH);
+      break;
+    default:
+      break;
+  }
 }
 
 void loop() {
@@ -23,15 +80,24 @@ void loop() {
         switch (command)
         {
           case 0x18:
-            Serial.print("UP");
+            driveMotor(1);
             break;
           case 0x52:
-            Serial.print("DOWN");
+            driveMotor(2);
             break;
-          case 
-        }
+          case 0x8:
+            driveMotor(3);
+            break;
+          case 0x5a:
+            driveMotor(4);               
+            break;
 
-        
+          default:
+            driveMotor(50);
+            break;
+        }      
+
+
 
         // Print the full raw data
         //IrReceiver.printIRResultShort(&Serial); 
